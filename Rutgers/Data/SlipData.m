222654d2fa0c236b28330ec900ee7fd2d7e2381e
Chunchu <cxz483@case.edu>
2022-04-03 19:37:59 -0400

    Update SlipData.m

--- Rutgers/Data/SlipData.m
+++ Rutgers/Data/SlipData.m
@@ -1,3 +1,8 @@
 %% Slip detection data analysis
 r_heel = data20220401170456(:,53:60);
 l_heel = data20220401170456(:,62:69);
+freq = data20220401170456(:,1);
+r_gaitStage = data20220401170456(:,3); 
+l_gaitStage = data20220401170456(:,4);
+slipRight = data20220401170456(:,5);
+slipLeft = data20220401170456(:,6);
